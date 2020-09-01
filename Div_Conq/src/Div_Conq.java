af84acdb2ce48da9cdc609a5d3e2a105b7cf3130
nicolasmss <nicolas.moraes@edu.pucrs.br>
2020-09-01 20:01:10 -0300

    Falta ex5

--- Div_Conq/src/Div_Conq.java
+++ Div_Conq/src/Div_Conq.java
@@ -1,21 +1,24 @@
 import java.util.LinkedList;
 import java.util.List;
 
+import static java.lang.Integer.max;
+
 public class Div_Conq {
     public static void main(String[] args) {
-        int[] lista = new int[7];
-        lista[0] = 3;
-        lista[1] = 5;
-        lista[2] = 4;
-        lista[3] = 1;
-        lista[4] = 6;
-        lista[5] = 5;
-        lista[6] = 3;
-        Div_Conq div_conq = new Div_Conq();
-        lista = div_conq.mergeSort(lista);
-        for (int i = 0;i<lista.length;i++){
-            System.out.print(lista[i]);
+        int n = 32;
+        int[] vetor = new int[n];
+        for (int i =0;i<n;i++){
+            vetor[i]= (int) (Math.random()*100);
         }
+        Div_Conq div_conq = new Div_Conq();
+        vetor = div_conq.mergeSort(vetor);
+        for (int i = 0;i<vetor.length;i++){
+            System.out.print("["+vetor[i]+"]");
+        }
+        int max2 = div_conq.maxVal2(vetor,0,5);
+        System.out.println("\n"+max2);
+
+        System.out.println(div_conq.multiply(13,12,4));
     }
 
     public int[] mergeSort(int[] vetor){
@@ -67,6 +70,43 @@ public class Div_Conq {
         //System.arraycopy(b,j,lista,i+j,b.length-1);
 
         return lista;
+    }
+
+    public int maxVal1(int[] a, int n){
+        int max = a[0];
+        for (int i=1;i<n;i++){
+            if (a[i]>max)
+                max=a[i];
+        }
+        return max;
+    }
+
+    public int maxVal2(int[]a,int init,int end){
+        if (end-init<=1)
+            return max(a[init],a[end]);
+        else{
+            int m = (init+end)/2;
+            int v1 = maxVal2(a,init,m);
+            int v2 = maxVal2(a,m+1,end);
+            return max(v1,v2);
+        }
+    }
+
+    public long multiply(long x,long y,long n){
+        if (n==1)
+            return x*y;
+        else{
+            long m = n/2;
+            long a = (long) (x/(Math.pow(2,m)));
+            long b = (long) (x%(Math.pow(2,m)));
+            long c = (long) (y/(Math.pow(2,m)));
+            long d = (long) (y%(Math.pow(2,m)));
+            long e = multiply(a,c,m);
+            long f = multiply(b,d,m);
+            long g = multiply(b,c,m);
+            long h = multiply(a,d,m);
+            return (long) ((Math.pow(2,2*m))*e + (Math.pow(2,m))*(g+h)+f);
+        }
     }
 
 }
